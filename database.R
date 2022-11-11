f20ddd09d1b2dee0e1c65701dbdac399ea948694
Pedro Diniz Guglielmeli <pedrodinizgu@gmail.com>
2022-11-10 22:41:53 -0300

    Added databases and started the main app
    Added 4 auxiliary databases, and started the city, age, sex and name generators.

---
+++ database.R
@@ -0,0 +1,140 @@
+# ===================================================================
+#
+#   UFTM BioStat DataBase
+#
+#     database.R
+#
+#     By: Pedro Diniz Guglielmeli <pedrodinizgu@gmail.com>
+#     https://github.com/paulk2jonas/UFTM-BioStat-DataBase
+#
+#   TODO:
+#     Add friend easter eggs
+#     Remove city_index
+#
+# ===================================================================
+
+# --------------------------------- Libraries -------------------------------- #
+library(tidyverse)
+library(readxl)
+# library(here)
+
+n <- 1000
+seed <- 42
+
+city_list <- read_excel("./UFTM-BioStat-DataBase/Background_Data/cities.xlsx")
+city_list$population <- as.numeric(city_list$population)
+# To save on processing power later on
+city_list_total_pop <- sum(city_list$population)
+
+city <- c()
+state <- c()
+# Because, otherwise, I can't get the cities to match withe their states
+# seq_len used because the lint warned about a possible edge case
+set.seed(seed)
+city_index <- sample(x = seq_len(nrow(city_list)),
+                     size = n,
+                     replace = TRUE,
+                     prob = city_list$population / city_list_total_pop)
+for (i in 1:n) {
+  city[i] <- city_list$city[city_index[i]]
+  state[i] <- city_list$state[city_index[i]]
+}
+
+state_initials <- c("Acre" = "AC",
+                    "Alagoas" = "AL",
+                    "Amapá" = "AP",
+                    "Amazonas" = "AM",
+                    "Bahia" = "BA",
+                    "Ceará" = "CE",
+                    "Espírito Santo" = "ES",
+                    "Goiás" = "GO",
+                    "Maranhão" = "MA",
+                    "Mato Grosso" = "MT",
+                    "Mato Grosso do Sul" = "MS",
+                    "Minas Gerais" = "MG",
+                    "Pará" = "PA",
+                    "Paraíba" = "PB",
+                    "Paraná" = "PR",
+                    "Pernambuco" = "PE",
+                    "Piauí" = "PI",
+                    "Rio de Janeiro" = "RJ",
+                    "Rio Grande do Norte" = "RN",
+                    "Rio Grande do Sul" = "RS",
+                    "Rondônia" = "RO",
+                    "Roraima" = "RR",
+                    "Santa Catarina" = "SC",
+                    "São Paulo" = "SP",
+                    "Sergipe" = "SE",
+                    "Tocantins" = "TO",
+                    "Distrito Federal" = "DF")
+
+city_state <- paste(city, " (", state_initials[state], ")", sep = "")
+
+age_sex_by_city <- read_excel("./UFTM-BioStat-DataBase/Background_Data/age.xlsx")
+
+
+# TODO: computar antes os sexos por cidade; 
+sex <- c()
+set.seed(seed)
+for (i in 1:n) {
+  if (city_state[i] %in% unique(age_sex_by_city$city)) {
+    sex[i] <- sample(x = c("M", "F"),
+                     size = 1,
+                     prob = c(sum(filter(age_sex_by_city,
+                                         city == city_state[i])$male),
+                     sum(filter(age_sex_by_city,
+                                         city == city_state[i])$female)))
+  } else {
+    sex[i] <- sample(x = c("M", "F"),
+                     size = 1,
+                     prob = c(105792687, 108954822))
+                     #! Qual a fonte disso?
+  }
+}
+
+age <- c()
+set.seed(seed)
+for (i in 1:n) {
+  age[i] <- sample(x = c("M", "F"),
+                   size = 1,
+                   prob = c(filter(age_sex_by_city, city == city_state[i], )))
+}
+
+set.seed(seed)
+sex <- sample(c("M", "F"),
+              size = n,
+              replace = TRUE,
+              prob = c(105792687, 108954822) / 214747509)
+
+forename_list <- read.csv("./UFTM-BioStat-DataBase/Background_Data/first_name.csv") %>%
+  select(1:4)
+
+male_names <- filter(forename_list, classification == "M") %>%
+  select(-c(2:3))
+female_names <- filter(forename_list, classification == "F") %>%
+  select(-c(2, 4))
+
+first_name <- c()
+set.seed(seed)
+for (i in 1:n) {
+  if (sex[i] == "M") {
+    first_name[i] <- sample(male_names$name,
+                         size = 1,
+                         prob = male_names$frequency_male / sum(male_names$frequency_male))
+  } else if (sex[i] == "F") {
+    first_name[i] <- sample(female_names$name,
+                         size = 1,
+                         prob = female_names$frequency_female / sum(female_names$frequency_female))
+  }
+}
+first_name <- str_to_title(first_name)
+
+surname_list <- read_excel("./UFTM-BioStat-DataBase/Background_Data/last_name.xlsx")
+
+set.seed(seed)
+last_name <- sample(surname_list$Surname,
+                    size = n,
+                    replace = TRUE,
+                    prob = surname_list$Incidence / sum(surname_list$Incidence))
+
+full_name <- paste(first_name, last_name, sep = " ")
